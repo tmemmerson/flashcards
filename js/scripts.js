d26bf7c4a1359d70f5eff69c4cdc1dac68e7cd45
Tristan Emmerson <tristan@stickerslug.com>
2020-06-02 15:34:28 -0700

    Add container and jumbotron with divs but they are not displaying properly yet.

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $(".clickJava").click(function() {
+    $("#javaName").toggle();
+    $("#javaDefinition").toggle();
+  })
+
+  $(".clickOperator").click(function() {
+    $("#operatorName").toggle();
+    $("#operatorDefinition").toggle();
+  })
+
+  $(".clickVariable").click(function() {
+    $("#variableName").toggle();
+    $("#variableDefinition").toggle();
+  })
+  
+  $(".clickVariableNamingConventions").click(function() {
+    $("#variableNamingConventionsName").toggle();
+    $("#variableNamingConventionsDefinition").toggle();
+  })
+
+  $(".clickFunctions").click(function() {
+    $("#functionsName").toggle();
+    $("#functionsDefinition").toggle();
+  })
+
+  $(".clickMethods").click(function() {
+    $("#methodsName").toggle();
+    $("#methodsDefinition").toggle();
+  })
+
+  $(".clickArguments").click(function() {
+    $("#argumentsName").toggle();
+    $("#argumentsDefinition").toggle();
+  })
+
+  $(".clickParameters").click(function() {
+    $("#parametersName").toggle();
+    $("#parametersDefinition").toggle();
+  })
+
+  $(".clickReturn").click(function() {
+    $("#returnName").toggle();
+    $("#returnDefinition").toggle();
+  })
+
+  $(".clickChainingMethods").click(function() {
+    $("#chainingMethodsName").toggle();
+    $("#chainingMethodsDefinition").toggle();
+  })
+
+  $(".clickStrings").click(function() {
+    $("#stringsName").toggle();
+    $("#stringsDefinition").toggle();
+  })
+
+  $(".clickBooleans").click(function() {
+    $("#booleansName").toggle();
+    $("#booleansDefinition").toggle();
+  })
+
+  $(".clickUndefined").click(function() {
+    $("#undefinedName").toggle();
+    $("#undefinedDefinition").toggle();
+  })
+
+  $(".clickNaN").click(function() {
+    $("#naNName").toggle();
+    $("#naNDefinition").toggle();
+  })
+
+  $(".clickNull").click(function() {
+    $("#nullName").toggle();
+    $("#nullDefinition").toggle();
+  })
+
+  $(".clickAlert").click(function() {
+    $("#alertName").toggle();
+    $("#alertDefinition").toggle();
+  })
+
+  $(".clickComments").click(function() {
+    $("#commentsName").toggle();
+    $("#commentsDefinition").toggle();
+  })
+
+  $(".clickjQuery").click(function() {
+    $("#jQueryName").toggle();
+    $("#jQueryDefinition").toggle();
+  })
+
+  $(".clickAttribute").click(function() {
+    $("#attributeName").toggle();
+    $("#attributeDefinition").toggle();
+  })
+})
